06db72f622c2fb7a02f49bb43b49e0a64492135c
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-18 17:53:35 +0100

    [feat] add article and insert state of new article

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
+// Array di articoli
+const Article = ['il miei viaggi', 'i miei pensieri', 'le mie recensioni']
 
 export default function input() {
 
-    const [listArticle, setListAricle] = useState('Articolo del Blog');
+    // Stato dell'articolo
+    const [listArticle, setListAricle] = useState(Article);
+    // Inserimento nuovo articolo
+    const [newArticle, setNewArticle] = useState('');
 
     //preventDefault per non inviare il Form
     const handleSubmit = event => {
@@ -12,8 +17,8 @@ export default function input() {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={listArticle}
-                onChange={e => { setListAricle(e.target.value) }} />
+            <input type="text" value={newArticle}
+                onChange={e => { setNewArticle(e.target.value) }} />
             <button>INVIA</button>
         </form>
     )
